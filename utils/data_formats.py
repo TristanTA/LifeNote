0306cc34c9c2cf6da59247a6df223eabf51a2469
TristanTA <all20052@byui.edu>
2025-11-03 16:48:38 -0700

    clear pt 2

--- utils/data_formats.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import List
-
-class ClassificationResult(BaseModel):
-    folder_path: str
-    new_folders: List[str] = Field(default_factory=list)
-    tags: List[str] = Field(default_factory=list)
