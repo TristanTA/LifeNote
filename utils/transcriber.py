07fe43253ab85303b164c72295e087ed8686fd2f
TristanTA <all20052@byui.edu>
2025-11-03 16:46:58 -0700

    clear house

--- utils/transcriber.py
+++
@@ -1,7 +0,0 @@
-from faster_whisper import WhisperModel
-
-model = WhisperModel("base", compute_type="int8")
-
-def transcribe_audio(file_path):
-    segments, _ = model.transcribe(file_path)
-    return " ".join([seg.text for seg in segments])
