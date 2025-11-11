340a9b0e0b17e246b3f092f178781d79590b4022
TristanTA <all20052@byui.edu>
2025-11-10 20:35:38 -0700

    Clean house, restart

--- schemas/session_class.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-from schemas.text_class import TextInput
-
-class Session:
-    def __init__(self):
-        self.session_id: int = int(uuid.uuid4())
-        self.text_inputs: list[TextInput] = []
--- schemas/text_class.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timezone
-import uuid
-
-class TextInput:
-    def __init__(self, content: str):
-        self.id: int = int(uuid.uuid4())
-        self.timestamp = datetime.now(timezone.utc).astimezone().isoformat(timespec="seconds")
-        self.content: str = content
